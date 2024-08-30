12528699d6fc013b6cf9c454158407154c26be32
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-08-30 19:48:22 +0000

    ok

--- main_atv2.py
+++ main_atv2.py
@@ -1,18 +1,18 @@
 
 import pandas as pd
 import seaborn as sns
-import csv
 import streamlit as st
 import matplotlib.pyplot as plt
 
 df = pd.read_csv(r'/workspaces/STR_TP2/Dados/HIST_PAINEL_COVIDBR_2024_Parte2_24ago2024.csv', delimiter=';', encoding='UTF-8')
-df['data'] 
 df['data'] = pd.to_datetime(df['data'])
 df['mes'] = df['data'].dt.to_period('M')
-df_rj = df.groupby('mes')['casosNovos'].sum().reset_index()
+df_plot = df.groupby('mes')['casosNovos'].sum().reset_index()
+
+st.write('### Casos Novos de COVID-19 no RJ por Mês')
+st.write('Selecionado o estado do RJ por ser o meu local de residência')
 
 plt.figure(figsize=(6, 4))
-sns.barplot(x = df_rj['mes'], y = df_rj['casosNovos'])
+sns.barplot(x = df_plot['mes'], y = df_plot['casosNovos'])
 plt.xticks(rotation=45)
-plt.title('Casos Novos de COVID-19 no RJ por Data')
 st.pyplot(plt)
